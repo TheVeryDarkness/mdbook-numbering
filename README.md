54a4d0cf6aa68ecc4fae5698364ad19151d62190
TheVeryDarkness <3266343194@qq.com>
2025-11-24 23:27:20 +0800

    Add a basic implementation

---
+++ README.md
@@ -0,0 +1,7 @@
+# mdbook-numbering
+
+A mdBook preprocessor that adds numbering.
+
+- [ ] Adds numbers to chapter titles
+- [ ] Configurable numbering styles (e.g., "1.", "1.1.", "I.", "A.", etc.)
+- [ ] Adds numbers to lines in code blocks.
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 //! This is a demonstration of an mdBook preprocessor which parses markdown
 //! and removes any instances of emphasis.
 
-use mdbook::book::{Book, Chapter};
+use mdbook::BookItem;
+use mdbook::book::Book;
 use mdbook::errors::Error;
 use mdbook::preprocess::{CmdPreprocessor, Preprocessor, PreprocessorContext};
-use mdbook::BookItem;
-use pulldown_cmark::{Event, Parser, Tag, TagEnd};
+use pulldown_cmark::{CowStr, Event, Tag, TagEnd::Heading};
 use std::io;
 
 fn main() {
@@ -28,51 +28,66 @@ fn main() {
     }
 }
 
-struct RemoveEmphasis;
+struct NumberingPreprocessor;
 
-impl Preprocessor for RemoveEmphasis {
+impl Preprocessor for NumberingPreprocessor {
     fn name(&self) -> &str {
-        "remove-emphasis"
+        "numbering"
     }
 
     fn run(&self, _ctx: &PreprocessorContext, mut book: Book) -> Result<Book, Error> {
-        let mut total = 0;
         book.for_each_mut(|item| {
-            let BookItem::Chapter(ch) = item else {
-                return;
-            };
+            let BookItem::Chapter(ch) = item else { return };
             if ch.is_draft_chapter() {
                 return;
             }
-            match remove_emphasis(&mut total, ch) {
-                Ok(s) => ch.content = s,
-                Err(e) => eprintln!("failed to process chapter: {e:?}"),
-            }
+            let Some(a) = &ch.number else { return };
+            let c = &ch.content;
+            let tokenized = mdbook::utils::new_cmark_parser(c, false);
+
+            let mut in_heading = false;
+
+            let mut stack = a.clone();
+
+            let events = tokenized.map(|event| match event {
+                Event::Start(Tag::Heading { level, .. }) => {
+                    in_heading = true;
+                    if level as usize > stack.len() {
+                        eprintln!("Warning: Heading level {} found, but only {} levels in numbering {:?} for chapter '{}'.", level, stack.len(), a, ch.name);
+                    }
+                    while level as usize >= stack.len() {
+                        stack.push(0);
+                    }
+                    while (level as usize) < stack.len() {
+                        stack.pop();
+                    }
+                    if stack.len() > a.len() {
+                        stack[level as usize - 1] += 1;
+                    }
+                    event
+                }
+                Event::Text(s) if in_heading => {
+                    let new_content = format!("{stack} {s}");
+                    Event::Text(CowStr::from(new_content))
+                }
+                Event::End(Heading(_)) => {
+                    in_heading = false;
+                    event
+                }
+                _ => event,
+            });
+
+            let mut buf = String::with_capacity(c.len() + a.to_string().len());
+            pulldown_cmark_to_cmark::cmark(events, &mut buf).expect("cmark parsing failed");
+
+            ch.content = buf;
         });
-        eprintln!("removed {total} emphasis");
         Ok(book)
     }
 }
 
-// ANCHOR: remove_emphasis
-fn remove_emphasis(num_removed_items: &mut usize, chapter: &mut Chapter) -> Result<String, Error> {
-    let mut buf = String::with_capacity(chapter.content.len());
-
-    let events = Parser::new(&chapter.content).filter(|e| match e {
-        Event::Start(Tag::Emphasis) | Event::Start(Tag::Strong) => {
-            *num_removed_items += 1;
-            false
-        }
-        Event::End(TagEnd::Emphasis) | Event::End(TagEnd::Strong) => false,
-        _ => true,
-    });
-
-    Ok(pulldown_cmark_to_cmark::cmark(events, &mut buf).map(|_| buf)?)
-}
-// ANCHOR_END: remove_emphasis
-
 pub fn handle_preprocessing() -> Result<(), Error> {
-    let pre = RemoveEmphasis;
+    let pre = NumberingPreprocessor;
     let (ctx, book) = CmdPreprocessor::parse_input(io::stdin())?;
 
     let processed_book = pre.run(&ctx, book)?;
