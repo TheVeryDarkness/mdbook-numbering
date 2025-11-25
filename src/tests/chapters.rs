7bc8d635d791ac944f8c3756624e5618e4114d39
TheVeryDarkness <3266343194@qq.com>
2025-11-25 22:35:28 +0800

    Update tests

--- src/tests/chapters.rs
+++ src/tests/chapters.rs
@@ -141,6 +141,122 @@ More content."
 }
 
 #[test]
+fn level_2_consecutive() {
+    let chapter = Chapter {
+        name: "Chapter 1".to_string(),
+        content: "\
+## Heading 1
+
+### Heading 2
+
+Some content.
+
+### Heading 3
+
+More content.
+"
+        .to_string(),
+        number: Some(SectionNumber(vec![1, 2])),
+        path: Some("chapter_1.md".into()),
+        ..Default::default()
+    };
+    let mut item = BookItem::Chapter(chapter);
+
+    NumberingPreprocessor::render_book_item(
+        &mut item,
+        &NumberingConfig {
+            code: CodeConfig { enable: false },
+            command: (),
+            heading: HeadingConfig {
+                enable: true,
+                numbering_style: NumberingStyle::Consecutive,
+            },
+            optional: (),
+        },
+        |err| panic!("{err}"),
+    );
+
+    assert_eq!(
+        item,
+        BookItem::Chapter(Chapter {
+            name: "Chapter 1".to_string(),
+            content: "\
+## 1.2. Heading 1
+
+### 1.2.1. Heading 2
+
+Some content.
+
+### 1.2.2. Heading 3
+
+More content."
+                .to_string(),
+            number: Some([1, 2].into_iter().collect()),
+            path: Some("chapter_1.md".into()),
+            ..Default::default()
+        }),
+    );
+}
+
+#[test]
+fn level_2_top() {
+    let chapter = Chapter {
+        name: "Chapter 1".to_string(),
+        content: "\
+# Heading 1
+
+## Heading 2
+
+Some content.
+
+## Heading 3
+
+More content.
+"
+        .to_string(),
+        number: Some(SectionNumber(vec![1, 2])),
+        path: Some("chapter_1.md".into()),
+        ..Default::default()
+    };
+    let mut item = BookItem::Chapter(chapter);
+
+    NumberingPreprocessor::render_book_item(
+        &mut item,
+        &NumberingConfig {
+            code: CodeConfig { enable: false },
+            command: (),
+            heading: HeadingConfig {
+                enable: true,
+                numbering_style: NumberingStyle::Top,
+            },
+            optional: (),
+        },
+        |err| panic!("{err}"),
+    );
+
+    assert_eq!(
+        item,
+        BookItem::Chapter(Chapter {
+            name: "Chapter 1".to_string(),
+            content: "\
+# 1.2. Heading 1
+
+## 1.2.1. Heading 2
+
+Some content.
+
+## 1.2.2. Heading 3
+
+More content."
+                .to_string(),
+            number: Some([1, 2].into_iter().collect()),
+            path: Some("chapter_1.md".into()),
+            ..Default::default()
+        }),
+    );
+}
+
+#[test]
 #[should_panic = "\
     Heading level h3 found, \
     but only 1 levels in numbering \"1.\" for chapter \"Chapter 1\".\
@@ -173,22 +289,5 @@ Some content."
             optional: (),
         },
         |err| panic!("{err}"),
-    );
-
-    assert_eq!(
-        item,
-        BookItem::Chapter(Chapter {
-            name: "Chapter 1".to_string(),
-            content: "\
-# 1. Heading 1
-
-### 1.0.1. Heading 2
-
-Some content."
-                .to_string(),
-            number: Some([1].into_iter().collect()),
-            path: Some("chapter_1.md".into()),
-            ..Default::default()
-        }),
     );
 }
