c0b6b90d5e335120c02fb947a85449c8ecd1ced8
TheVeryDarkness <TheVeryDarkness@users.noreply.github.com>
2025-12-06 06:54:01 +0000

    deploy: 16468b4d175a1e50015f2d7db04a03df43d9dc04

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-110d2a50.js',
+                path_to_root + 'searchindex-65fd22e4.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
